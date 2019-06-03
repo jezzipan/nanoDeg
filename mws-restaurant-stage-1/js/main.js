daa50b29de9b83e8db8c7020252089b5fa47da51
Jessica <jessica.leute@gmail.com>
2019-06-03 12:33:07 -0300

    acess token

--- mws-restaurant-stage-1/js/main.js
+++ mws-restaurant-stage-1/js/main.js
@@ -78,7 +78,7 @@ initMap = () => {
         scrollWheelZoom: false
       });
   L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.jpg70?access_token={mapboxToken}', {
-    mapboxToken: 'pk.eyJ1IjoicGFuc2h1ZyIsImEiOiJjanRmdnBqOWExeGpwNDNvYnQ4MjZhdDJzIn0.eqBE2reP-3V3GLAg2pyXrg',
+    mapboxToken: 'pk.eyJ1IjoicGFuc2h1ZyIsImEiOiJjanczemtxeDgwdmdiNGFuend3ZTBvMDFyIn0.EscqOjZOxeGHys5MiAAoWg',
     maxZoom: 18, 
     attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, ' +
       '<a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, ' +
